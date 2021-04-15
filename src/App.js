d37e3069bb7ffa8a93c65db78b6af94bfd345777
user <kamauri84@gmail.com>
2021-04-16 07:24:30 +1000

    Create a Star Rating component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import StarRating from './StarRating';
 
 
 function App() {
   return (
     <div className="App">
-   
+   <StarRating />
     </div>
   );
 }
---
+++ src/StarRating.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StarRating = () => {
+    return (
+        <div>
+            star
+        </div>
+    )
+}
+
+export default StarRating
