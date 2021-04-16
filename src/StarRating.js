c1e782c58c099e8f14a95938bd16079d3ae6ddaf
user <kamauri84@gmail.com>
2021-04-16 11:40:59 +1000

    conditionally render feedback

--- src/StarRating.js
+++ src/StarRating.js
@@ -20,31 +20,37 @@ const StarRating = () => {
                     {[...Array(5)].map((star,index) =>{
                         const ratingValue = index + 1;
                         return(
-                            <label>
-                            <input 
-                                type="radio" 
-                                name="rating"
-                                value={ratingValue} 
-                                onClick={() => setRating(ratingValue)}
-                                />
-                            <bsIcons.BsFillStarFill 
-                                className="star" 
-                                size="100" 
-                                color={ratingValue <= (hover|| rating)? "#ffc107" : "#e4e5e9" }
-                                onMouseEnter ={() => setHover(ratingValue)}
-                                onMouseLeave ={() => setHover(null)}
-                                />
-                        </label>
+                            <div key={index}>
+                                <label>
+                                <input 
+                                    type="radio" 
+                                    name="rating"
+                                    value={ratingValue} 
+                                    onClick={() => setRating(ratingValue)}
+                                    />
+                                <bsIcons.BsFillStarFill 
+                                    className="star" 
+                                    size="100" 
+                                    color={ratingValue <= (hover|| rating)? "#ffc107" : "#e4e5e9" }
+                                    onMouseEnter ={() => setHover(ratingValue)}
+                                    onMouseLeave ={() => setHover(null)}
+                                    />
+                                </label>
+                            </div>
                         )
                     
                     })}
                 </div>
-                <div className="container flex">
-                <tiIcons.TiTick size="60"/>         
-                <p>Thanks! You rated this {rating} stars. Would you like to add feedback?</p>
-                </div>
-                <textarea type="text" cols="35" rows="5" placeholder="Enter feedback here . :)"/>
-                <button type="submit" onSubmit={handleSubmit}>Submit</button>
+                {rating ? (
+                <>
+                    <div className="container flex">
+                    <tiIcons.TiTick size="60"/>         
+                    <p>Thanks! You rated this {rating} stars. Would you like to add feedback?</p>
+                    </div>
+                    <textarea type="text" cols="35" rows="5" placeholder="Enter feedback here . :)"/>
+                    <button type="submit" onSubmit={handleSubmit}>Submit</button>
+                </>): null}
+                
             </form>
             
         </div>
